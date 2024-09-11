c8c3f258a0c13f0f146f01a77efe050c8a8e48ab
santhan29 <santhanpeddaguravagari@gmail.com>
2024-09-11 13:00:42 +0530

    terraform

---
+++ variables/variables.tf
@@ -0,0 +1,49 @@
+variable "ami_id" {
+    type = string 
+    default = "ami-09c813fb71547fc4f"
+    description = " ami id of devops rhel9 "
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "tags" {
+    type = map
+    default = {
+        Name = "backend"
+        Project = "expense"
+        Component = "backend"
+        Environment = "DEV"
+        Terraform = "true"
+    }
+}
+
+variable "sg_name" {
+    default = "allow_sshh" 
+
+}
+
+variable "sg_description" {
+    default = "Allow port number 22 for ssh access"
+}
+
+variable "from_port" {
+    default = 22
+    type = number
+}
+
+variable "to_port" {
+    default = 22
+    type = number
+}
+
+variable "protocol" {
+    default = "tcp"
+}
+
+variable "ingress_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
