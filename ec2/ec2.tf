06c420e75f2f814a13cea89b547305433db3c30f
santhan29 <santhanpeddaguravagari@gmail.com>
2024-12-02 16:54:47 +0530

    ec2

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "terraform" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
-      Name = "terraform"
+      Name = "node-1"
   }
 
 }
